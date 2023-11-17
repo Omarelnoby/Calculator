fc38c2e64aa68af42ca7ada52840488831d1595a
ELNOBY <omarelnobhy@gmail.com>
2023-11-17 22:25:35 +0200

    ELNOBY:the main func is created

--- calculator.c
+++ calculator.c
@@ -1,5 +1,16 @@
 #include "calculator.h"
-
+int main(){
+	int x,y,result;
+	printf("enter the value of the first number\n");
+	scanf ("%d",&x);
+	printf("enter the value of the second number\n");
+	scanf ("%d",&y);	
+	printf("the addition result ")
+	
+	
+	
+	return 0;
+}
 int add(int a, int b)
 {
 	return a+b;
@@ -19,10 +30,20 @@ int div(int x,int y){
 		return x/y;
 	}
 
-
+}
 int mult(int x, int y)
 {
 	int z = x*y;
 	return z;
+	
+}
+int modulo (int x, int y){
+	
+	if (y==0){
+		return 0;
+	}		
+	else{
+		return x%y;		
+	}
 	
 }
